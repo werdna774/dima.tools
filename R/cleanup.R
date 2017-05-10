7348e6c1d8341c3a7ebf3ec657969feda032f705
nstauffer <ngstauffer@gmail.com>
2017-05-10 06:48:58 -0600

    Update comments

--- R/cleanup.R
+++ R/cleanup.R
@@ -4,11 +4,14 @@ tidy.stability <- function(dataframe){
     class(dataframe[, X]) %>% grepl(pattern = "logical")
   }) %>% .[grepl(x = names(.), pattern = "^Veg")] %>% .[unlist(.)] %>% names()
   bad.columns <- dataframe[, offenders]
+  ## This replaces all the F values with "F" values
   bad.columns[!bad.columns] <- "F"
+  ## Replace the columns in the original input with the corrected ones
   dataframe[, offenders] <- bad.columns
 
-  ## Get the header information
+  ## Get the header information to bind to data subsets in the lapply() later
   header <- dataframe %>% select(SiteID, SiteKey, PlotID, PlotKey, FormDate, Observer, Recorder, DataEntry, DataErrorChecking, BoxNum)
+
   ## Make sure that instead of 1:6 we have 1:18 for the Line[n] columns
   lines <- dataframe[, grepl(x = names(dataframe), pattern = "Line[1-6]")]
   lines <- data.frame(
@@ -22,12 +25,12 @@ tidy.stability <- function(dataframe){
   names(lines) <- paste0("Line", 1:18)
   dataframe <- cbind(lines, dataframe[, !grepl(x = names(dataframe), pattern = "Line[1-6]")])
 
-  ## Get each subset of columns ending in 1 through 18 and bind them into an output
+  ## Get each subset of columns ending in 1 through 18, strip the numerical endings, and bind them into an output
   output <- lapply(1:18, function(X) {
     cbind(header,
           dataframe[, grep(x = names(dataframe), pattern = paste0("[a-z]", X, "$"))]
           ) %>% setNames(names(.) %>% stringr::str_replace(., pattern = paste0(X, "$"), replacement = ""))}
-  ) %>% dplyr::bind_rows(output)
+  ) %>% dplyr::bind_rows()
 
   return(output)
 }
