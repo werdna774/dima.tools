78c8cf1e8358f31bd7646182bba852384d290c68
nstauffer <ngstauffer@gmail.com>
2017-07-07 12:47:23 -0600

    Bring common.species() up to snuff and export

--- R/common.species.R
+++ R/common.species.R
@@ -1,10 +1,32 @@
-common.species <- function(dima.tables){
+#' Find the most common species characteristics assigned in DIMAs
+#' @description Compare two or more DIMAs against each other to find out what the variability in species characteristic assignment is like and what the most common assignments are. This pulls in data from DIMAs without combining them because species are expected to appear more than once and combined tables will wreak havoc with some \code{merge()} steps. The data frame \code{uniques} contains one entry per combination of species code, growth habit, growth sub habit, and duration and a count of how many databases that combination occurred in. The data frame \code{frequent} contains one entry for each species code that had at least one of those characteristics assigned and the most frequently occurring value for each characteristic. These are considered separately though, so if ARTRW8 shows up in three DIMAs as non-woody perennial forb, a woody perennial shrub, and a woody annual shrub then the entry in the output would call it a woody perennial shrub.
+#' @param data.path A string. The path to the folder containing the DIMA[s].
+#' @param dima.list Optional character vector. Two or more filenames of DIMAs to read data from in \code{data.path}. If not specified, all DIMAs in the folder will be read from.
+#' @return A list with containing data frames \code{frequent} and \code{unique}.
+#' @export
+common.species <- function(data.path,
+                           dima.list){
+  if (length(dima.list) < 2) {
+    stop("At least two DIMAs are required for this to work.")
+  }
+
+  ## Read in the tables from the DIMAs
+  dima.tables <- read.dima(data.path = data.path,
+                           dima.list = dima.list,
+                           combine = FALSE,
+                           custom.query = c("tblSpecies" = "SELECT * FROM tblSpecies",
+                                            "tblSpeciesGrowthHabit" = "SELECT * FROM tblSpeciesGrowthHabit"))
+
   ## Make a species characteristics data frame
-  species <- merge(x = dima.tables$tblSpecies,
-                   y = dima.tables$tblSpeciesGrowthHabit,
-                   by.x = "GrowthHabitCode",
-                   by.y = "Code",
-                   all.x = TRUE)
+  species <- lapply(dima.tables, FUN = function(X){
+    merge(x = X$tblSpecies,
+          y = X$tblSpeciesGrowthHabit,
+          by.x = "GrowthHabitCode",
+          by.y = "Code",
+          all.x = TRUE) %>%
+      dplyr::select(SpeciesCode, Duration, GrowthHabit, GrowthHabitSub)
+  }) %>% dplyr::bind_rows()
+
 
   species.uniques <- dplyr::select(species, SpeciesCode, Duration, GrowthHabit, GrowthHabitSub) %>%
     dplyr::distinct() %>% dplyr::group_by(SpeciesCode, SpeciesCode, Duration, GrowthHabit, GrowthHabitSub) %>%
@@ -29,6 +51,6 @@ common.species <- function(dima.tables){
     setNames(c("SpeciesCode", "Duration", "GrowthHabit", "GrowthHabitSub")) %>%
     .[!(is.na(.$Duration) & is.na(.$GrowthHabit) & is.na(.$GrowthHabitSub)),]
 
-  return(list(species.frequent = species.common.df,
-              species.uniques = species.uniques))
+  return(list(frequent = species.common.df,
+              uniques = species.uniques))
 }
